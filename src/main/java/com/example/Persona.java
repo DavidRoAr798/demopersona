253f191a018a523d9f9e00f432e14f339814d08f
David <david.rodr@gmail.com>
2016-07-12 10:38:38 +0200

    Initial commit

---
+++ src/main/java/com/example/Persona.java
@@ -0,0 +1,84 @@
+package com.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Locale;
+
+
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre, apellido;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Persona persona = (Persona) o;
+
+        if (getId() != null ? !getId().equals(persona.getId()) : persona.getId() != null) return false;
+        if (getNombre() != null ? !getNombre().equals(persona.getNombre()) : persona.getNombre() != null) return false;
+        if (getApellido() != null ? !getApellido().equals(persona.getApellido()) : persona.getApellido() != null)
+            return false;
+        return getAge() != null ? getAge().equals(persona.getAge()) : persona.getAge() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getNombre() != null ? getNombre().hashCode() : 0);
+        result = 31 * result + (getApellido() != null ? getApellido().hashCode() : 0);
+        result = 31 * result + (getAge() != null ? getAge().hashCode() : 0);
+        return result;
+    }
+}
+
---
+++ src/main/java/com/example/PersonaRepository.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PersonaRepository extends JpaRepository<Persona,Long> {
+
+    List<Persona> findByNombre(String nombre);
+
+    List<Persona> findByNombreAndApellido (String nombre, String apellido);
+
+    List<Persona> findByNombreOrApellido (String nombre, String apellido);
+
+    List<Persona> findByAgeGreaterThanEqual (Integer age);
+
+}
